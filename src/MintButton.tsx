f47f80a4853741fa983bfb995d0ee6405b34c39f
Bygood91 <c.miorin@free.fr>
2022-01-30 03:26:06 +0100

    Graphik Adjutements

--- src/MintButton.tsx
+++ src/MintButton.tsx
@@ -9,7 +9,7 @@ import {CandyMachine} from './candy-machine';
 export const CTAButton = styled(Button)`
   display: block !important;
   margin: 0 auto !important;
-  background-color: var(--title-text-color) !important;
+  background-color: var(--mint-text-color) !important;
   font-size: 1.2em;
 `;
 
